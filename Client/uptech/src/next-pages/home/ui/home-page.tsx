7494c741c9ce31f20f02ccd573733472d5c5e089
Edanriell <Edanriell@gmail.com>
2025-02-09 01:24:27 +0200

    feature(loader): working on loader

--- Client/uptech/src/next-pages/home/ui/home-page.tsx
+++ Client/uptech/src/next-pages/home/ui/home-page.tsx
@@ -1,9 +1,12 @@
 import { FC } from "react";
 
+import { Loader } from "@shared/ui/loader/ui";
+
 export const HomePage: FC = () => {
 	return (
 		<main className="relative z-[20]">
 			<h1>Home Page</h1>
+			<Loader />
 		</main>
 	);
 };
